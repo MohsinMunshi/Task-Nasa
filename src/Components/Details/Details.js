ef9455a9ebc2a0dfa53a6773d901c6d401351fe2
DESKTOP-DHNIVRQ\ABC <mohsin.munshi24@gmail.com>
2021-09-18 10:54:15 +0530

    Frist Commit

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,10 +1,63 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import {useParams,useLocation,useHistory} from 'react-router-dom'
+import * as API from "../../API";
+import styles from "./Styles";
+import {Container,Paper,Typography,Grid,Button,CircularProgress} from '@material-ui/core/';
 const Details = () => {
+    const classes = styles()
+
+    const {id} = useParams()
+    const location = useLocation()
+    const history = useHistory()
+    const path = location.pathname.split("/")[1]
+
+    const [asteroidData, setAsteroidData] = useState("")
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        AsteroidData()
+    }, []);
+
+    const AsteroidData = () =>{
+        API.getAsteroidData(id).then(({data})=>{
+            setAsteroidData(data)
+            setIsLoading(false)
+        })
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Container  component="main" maxWidth="xs" className={classes.root}>
+            <Paper className={classes.paper} elevation={3}>
+                {
+                    isLoading ? <CircularProgress />
+                        :
+                        <>
+                            <Typography variant="h5">
+                                Asteroid Details
+                            </Typography>
+                            <Grid container spacing={1}>
+                                <Typography variant="h3" class={classes.text}>
+                                    Name : {asteroidData.name}
+                                </Typography>
+                                <Typography variant="h3" class={classes.text}>
+                                    Nasa JPL URL : <a target="_blank" href={asteroidData.nasa_jpl_url} rel="noreferrer"> Click Here </a>
+                                </Typography>
+                                {path !== "random" ?<Typography variant="h3" class={classes.text}>
+                                    Is Potentially Hazardous Asteroid
+                                    : {asteroidData.is_potentially_hazardous_asteroid ? "Yes" : "No"}
+                                </Typography> : null}
+                            </Grid>
+                            <Button
+                                fullWidth variant="contained"
+                                className={classes.submit}
+                                onClick={()=>{history.push("/")}}
+                                    >
+                                Home
+                            </Button>
+                        </>
+                }
+            </Paper>
+        </Container>
     );
 };
 
